9d7b714f1ab643fbca0b8e9ad40771cf6132949c
丁宇翔 <tech32@hzdusun.com>
2018-06-15 16:19:03 +0800

    feat: 添加阿里飞燕平台sdk 1.env.sh 配置环境变量 2.sdk_packs 放置阿里释放出的飞燕平台sdk包，当前版本为20180614 3.ilop_sdk放置sdk包解压后的源码，make编译，目标文件为sdk库文件，置于lib内

---
+++ env.sh
@@ -0,0 +1,21 @@
+#!/bin/sh 
+
+OPENWRTDIR=/home/dingyuxiang/projects/openwrt
+TOOLCHAIN=${OPENWRTDIR}/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2
+TARGET=target-mipsel_24kec+dsp_uClibc-0.9.33.2
+KERNELDIR=${OPENWRTDIR}/build_dir/target-mipsel_24kec+dsp_uClibc-0.9.33.2/linux-ramips_mt7620/linux-3.18.20
+STAGING_DIR=${OPENWRTDIR}/staging_dir
+CROSS_COMPILE=${OPENWRTDIR}/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin/mipsel-openwrt-linux-
+CC=${OPENWRTDIR}/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin/mipsel-openwrt-linux-gcc
+ARCH=mips
+INC=-I${OPENWRTDIR}/staging_dir/target-mipsel_24kec+dsp_uClibc-0.9.33.2/usr/include/osi
+INC+=-I${OPENWRTDIR}/staging_dir/target-mipsel_24kec+dsp_uClibc-0.9.33.2/usr/include/dusun
+
+export ARCH
+export TOOLCHAIN
+export TARGET
+export STAGING_DIR
+export CROSS_COMPILE
+export KERNELDIR
+export CC
+export INC
